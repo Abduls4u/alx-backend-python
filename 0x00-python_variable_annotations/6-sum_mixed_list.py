c40d66fc10d60b08b53f8b14f23c820f96c01a99
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-10-23 21:13:52 +0100

    Add solution to task 5, 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 ''' 6-sum_mixed_list.py module '''
-from typing import List
+from typing import List, Union
 
 
-def sum_mixed_list(input_list: List[int | float]) -> float:
+def sum_mixed_list(input_list: List[Union[int, float]]) -> float:
     '''returns sum of list items'''
     sum = 0.0
     for i in input_list:
