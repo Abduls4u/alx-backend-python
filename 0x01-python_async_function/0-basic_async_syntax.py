0220a336182e89c280d6125014a86c502720c5e8
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-10-26 09:05:31 +0100

    Add solutions to task 0, 1

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import random
 from typing import Union
 
 
-async def wait_random(max_delay: int = 10) -> Union[int, float]:
+async def wait_random(max_delay: int = 10) -> float:
     '''async coroutine that returns random delay'''
     random_delay = random.uniform(0, max_delay)
     await asyncio.sleep(random_delay)
--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,6 +7,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''calls wait_random n times'''
     tasks = [wait_random(max_delay) for _ in range(n)]
     res = await asyncio.gather(*tasks)
     return (sorted(res))
