57695704875a7996830b4c36085622a97524a8d1
Abduls4u <abdulsalamabdulsomad14@gmail.com>
2023-11-16 22:02:20 +0100

    Add solution to tasks 0, 1, 2

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List:
+async def async_comprehension() -> List[float]:
     '''async comprehension'''
     return ([_ async for _ in async_generator()])
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''2-measure_runtime.py modules'''
 import asyncio
+from asyncio import gather
 from time import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
